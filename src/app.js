d2adbc1860bd58c44e80fff82f832d96be4354da
raghuharsh <raghuhasrh007@rocketmail.com>
2021-08-18 00:14:45 +0530

    jwt cookie

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const app = express();
 const hbs = require("hbs");
 const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
 
 
 const Register = require("./models/userRegistration")
@@ -17,6 +18,7 @@ const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser());
 
 app.set("view engine", "hbs");
 app.set("views", template_path);
@@ -26,6 +28,15 @@ console.log(process.env.SECRET_KEY)
 app.get("/", (req, res) => {
 
     res.render("index")
+
+})
+
+app.get("/home", (req, res) => {
+
+
+    console.log("cookie info", req.cookies.jwt);
+    // console.log("cookie info 2", cookies);
+    res.render("home")
 
 })
 
@@ -50,6 +61,12 @@ app.post("/register", async(req, res) => {
 
         if (pass == confirmpass) {
             const token = await user.generateToken();
+            res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 3000),
+                httpOnly: true,
+                // secure:true
+            });
+            console.log(cookie);
             const createUser = await user.save();
             res.status(201).render("index");
         } else {
@@ -75,6 +92,11 @@ app.post("/login", async(req, res) => {
         const useremail = await Register.findOne({ email: email });
         const passmatch = await bcrypt.compare(password, useremail.password);
         const token = await useremail.generateToken();
+        res.cookie("jwt", token, {
+            expires: new Date(Date.now() + 30000),
+            httpOnly: true,
+            // secure:true
+        });
         if (passmatch) {
             res.status(201).render("index");
         } else { res.send("invalid login details") }
